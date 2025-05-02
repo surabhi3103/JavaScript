914c220a23d4a7e8a179fe0765566da3e70f2162
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-02 18:15:53 +0000

    container config done

--- basics.js
+++ basics.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// console.log("hello");
+const accountId = 13344;
+let accountEmail ="hi@gmail.com"
+var accountPassword = 47585;
+let accountState;
+// accountId = 34; not allowed
+accountEmail = "hdd"
+accountPassword = 12345;
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword, accountState])
+// this prints all the values in the tabular structure
+
+// prefer not using var
