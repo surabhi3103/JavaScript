e652bec46cc8589751766f5c32b8b855841be993
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-03 18:01:47 +0000

    container config done

---
+++ 04_strings.js
@@ -0,0 +1,32 @@
+// const name  ="Surabhi";
+// const age = 22;
+// // console.log(name + age+ "Mungle");
+// // this is outdated
+// // new one is ${variable name}
+// // console.log(`${name} is ${age} years old`);
+
+const gameCount  = new String("Hel-lo")
+// another way to declare a string here it is an object
+console.log(gameCount);
+// console.log(gameCount[4]);
+// console.log(gameCount.__proto__.gameCount);
+
+// // see other function
+// console.log(gameCount.__proto__ === String.prototype); // true
+// console.log(gameCount.toLowerCase());
+// console.log(gameCount.charAt(2));
+// console.log(gameCount.length);
+// console.log(gameCount.indexOf("l"));
+// const game = gameCount.substring(0,3)
+// console.log(game);
+// const game2 = gameCount.slice(1,2)
+// console.log(game2);
+// const game3 = "     ggg    "
+// console.log(game3.trim());
+// // trim removes extra spaces that is whitespaces it also has start and end function
+// const url = "google@ghj.com"
+// console.log(url.replace('@ghj','@'));
+// console.log(url.includes('google'));
+// console.log(url.startsWith('google'));
+console.log(gameCount.split('-'));
+
---
+++ comparions.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+// console.log("2" > 1);
+// console.log("02" > 1);  here string gets converted into number automatically by js
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// === this checks the data type first and then the number
+console.log("2" === 2);
