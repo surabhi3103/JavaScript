af6611cfc90f624310e26eaab9e9638a687de8ab
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-05 15:16:30 +0000

    done

--- 19_20_functions.js
+++ 19_20_functions.js
@@ -39,5 +39,42 @@ function loginUserMessage(username){//pass username =any thing so that it will n
     }
     return `${username} is logged in`;
 }
-console.log(loginUserMessage("Surabhi"));
+// console.log(loginUserMessage("Surabhi"));
 // if no value is passed then it will be undefined not null
+
+// if there are multiple parameters
+function calculateCartPrice(...num1){//... is rest operator which will take all the values passed in the function
+    // also called as spread operator
+    return num1;
+}
+console.log(calculateCartPrice(10, 20, 30, 40, 50)); // here it will return an array of all the values passed
+
+
+// function calculateCartPrice(val1,val2,...num1){//... is rest operator which will take all the values passed in the function
+//     // also called as spread operator
+//     return num1;
+// }
+// console.log(calculateCartPrice(10, 20, 30, 40, 50)); // here it will return 30 40 50 as val1 is 10 and val2 is 20
+
+
+// // function using object
+
+// const user ={
+//     username : "Surabhi",
+//     password: "123456",
+// }
+// function handleJob(anyobject){
+//     console.log('Username is ${anyobject.username} and the price is ${anyobject.price}');
+    
+// }
+// handleJob(user); // here user is passed as object and the function will print the values of the object
+
+
+
+const myNewArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function returnSecondValue(getArray){
+    return getArray[1]; // here it will return the second value of the array
+}
+console.log(returnSecondValue(myNewArray)); // here it will return 2 as the second value of the array  
+console.log(returnSecondValue([1, 2, 3, 4, 5])); // here it will return 2 as the second value of the array
+  
