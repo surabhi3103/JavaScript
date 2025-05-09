8f036c42b85d639b60facb83afa6097893cac301
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-09 18:05:29 +0000

    done

--- 23_arrowfunction.js
+++ 23_arrowfunction.js
@@ -7,11 +7,55 @@ const user ={
         
     }
 }
-  user.welcomeMessage(); // Surabhi welcome to the world of JS
+  // user.welcomeMessage(); // Surabhi welcome to the world of JS
 // In the above code, we have created an object user with properties username and price.
 // We have also created a method welcomeMessage that uses the this keyword to refer to the username property of the object.
 // The this keyword is used to refer to the current object in which the method is defined.
 // In this case, the this keyword refers to the user object.
 
-user.username = "sam";
-user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
+// console.log(this);// if we run this command on;y it will return an empty object
+
+function one(){
+  // let username ="Surabhi";
+  // console.log(this);
+  // this will have alot of values you can see by running these individually 
+  // console.log(this.username);// undefined as output
+  
+}
+// one()
+
+// const one  = function(){
+//   let username ="Surabhi"
+//   console.log(this.username);
+// }
+// this will give the same output as earlier
+
+
+const chai = () => { // => is an arrow function 
+  let username ="Surabhi"
+  console.log(this.username);
+  // if we use this only without username it will give  {} as output
+} 
+// chai()
+
+// function mai jaake this use nhi kar parhe hai bs objects mai kar sakte hai 
+
+
+
+// Arrow function//
+// const addTwo =(num1,num2) => {
+//   return num1 +num2
+// }
+// console.log(addTwo(3,4));
+// implicit return
+// const addTwo =(num1,num2) => num1 +num2
+// const addTwo =(num1,num2) => (num1 +num2)
+// two ways to write
+// console.log(addTwo(9,5));
+const addTwo=(num1,num2) => ({username:"Surabhi"})
+console.log(addTwo(9,2));
+
+
+
---
+++ 24_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions(IIFE)
+// function likha immediately invoke hojaye
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// } 
+// chai()
+ 
+
+// how to run without chai()
+(function chai(){
+    console.log(`DB CONNECTED`);
+})()
+
