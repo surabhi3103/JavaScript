38331e3b8756fcbcf38f2f66f79670769b70a45a
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-12 11:40:56 +0000

    commit

--- 24_iife.js
+++ 24_iife.js
@@ -8,6 +8,25 @@
 
 // how to run without chai()
 (function chai(){
+    // function chai is a named IIFE
     console.log(`DB CONNECTED`);
-})()
+})();
+// semi colon is necessary to end the function
 
+(function aurcode(){
+    console.log(`DB connected two`);
+    
+})();
+
+
+// now using an arrow funtion also called simple IIFE
+(() => {
+    console.log(`DB connected Three`);
+    
+})();
+
+//  how to pass a name or other parameters
+((name) => {
+    console.log(`DB connected Three ${name}`);
+    
+})('Surabhi');
