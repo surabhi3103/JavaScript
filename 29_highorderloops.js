e7bc149199dea03d8f0bc577d4196859ab6ee030
Mungle Surabhi Sudhir <118480875+surabhi3103@users.noreply.github.com>
2025-05-14 19:02:42 +0000

    done

---
+++ 29_highorderloops.js
@@ -0,0 +1,17 @@
+// for of 
+["","",""] // empty string
+[{},{},{}] // empty object
+
+const arr =[1,2,3,4,5,6,7,8,9,10];
+ for (const num of arr) { // kis object pe loop lagana chahte hai(yha object ka matlab js wala obj nhi hai)
+console.log(num);
+    
+ }
+
+const myString =["hello","world","this","is","a","string"];
+for (const str of myString) {
+    console.log(str);
+}
+
+
+// for in
